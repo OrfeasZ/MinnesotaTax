f6d6ea3e7da938025e69c8fbf188630b59b7cf3d
OrfeasZ <orfeaz@gmail.com>
2017-01-10 22:04:35 +0200

    Removed unneeded methods from the new OutputSystem.

--- src/main/java/gr/uoi/cse/taxcalc/io/OutputSystem.java
+++ src/main/java/gr/uoi/cse/taxcalc/io/OutputSystem.java
@@ -38,28 +38,4 @@ public class OutputSystem {
 
         throw new IllegalArgumentException();
     }
-
-    public static void saveUpdatedTaxpayerTxtInputFile(String filePath, int taxpayerIndex){
-
-    }
-
-    public static void saveUpdatedTaxpayerXmlInputFile(String filePath, int taxpayerIndex){
-
-    }
-
-    public static void createTaxpayerReceiptsPieJFreeChart(int taxpayerIndex){
-
-    }
-
-    public static void createTaxpayerTaxAnalysisBarJFreeChart(int taxpayerIndex){
-
-    }
-
-    public static void saveTaxpayerInfoToTxtLogFile(String folderSavePath, int taxpayerIndex){
-
-    }
-
-    public static void saveTaxpayerInfoToXmlLogFile(String folderSavePath, int taxpayerIndex){
-
-    }
 }
